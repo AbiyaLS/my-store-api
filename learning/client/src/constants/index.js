d06f6bbb68014f0070d68d71cd4450636e212001
Ajesh <ajeshs.dev@gmail.com>
2026-01-25 20:21:53 +0530

    feat: product faq implemented

---
+++ learning/client/src/constants/index.js
@@ -0,0 +1,12 @@
+export const CATEGORIES = {
+  CARS: "cars",
+  SPORTS_CARS: "sports-cars",
+  MOTORCYCLES: "motorcycles",
+  BICYCLES: "bicycles",
+  BUSES: "buses",
+  LEISURE_BOATS: "leisure-boats",
+  YACHTS: "yachts",
+  VANS: "vans",
+  BUGGIES: "buggies",
+  CHARTERS: "charters",
+};
--- learning/client/src/pages/ProductDetailsPage.jsx
+++ learning/client/src/pages/ProductDetailsPage.jsx
@@ -2,8 +2,13 @@ export default function ProductDetailsPage() {
   return (
     <div>
       ProductDetailsPage
-      <div>image grid</div>
-      <p>description</p>
+      {/* image grid and details */}
+      <section>
+        <div>image grid</div>
+        <p>description</p>
+      </section>
+      {/* faq section */}
+      <section>Product FAQ</section>
     </div>
   );
 }
